096974491e49a62d05a7ab0b7ee5490936b20542
AdelaRomeroV <ajrvjarinee17@gmail.com>
2024-04-08 18:41:30 -0500

    Crear Proyecto

---
+++ Ejercicio 01 - semana 04/Ejercicio 01 - semana 04/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_01___semana_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
